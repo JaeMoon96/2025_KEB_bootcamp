21306dae35715e7b000da9ab513646fac7dccf62
JaeMoon <ljm96040@gmail.com>
2025-02-11 13:39:52 +0900

    v1.0 day02 prime number

--- day02.py
+++ day02.py
@@ -1,64 +1,26 @@
-from operator import truediv
-
-n = int(input("Input number : "))
-is_prime = True
-
-if n >=2:
-     for i in range(2 , n):
-         if n % i == 0:
-            is_prime = False #count = count +1
-            break
-
-        else
-        is_prime = True
-
-        if count == 0:
-            print(f"{n} is prime number")
-        else:
-            print(f"{n} is NOT prime number!")
-
-
-n = int(input("Input number: "))
-is_prime = True
-
-if n >= 2:
-    for i in range(2, n):
-        if n % i == 0:
-            is_prime = False
-            break  # 약수가 발견되면 즉시 종료
-
-    if is_prime:
-        print(f"{n} is a prime number")
+def is_prime(num) -> bool:
+    """
+    A function that returns True if it is a prime number and False if it is not a prime number
+    :param num: integer number
+    :return: boolean type
+    """
+    if num >= 2:
+        for i in range(2, int(num ** 0.5) + 1):
+            if num % i == 0:
+                return False
+                #is_prime = False
+                #break
+            #print(i, end=' ')
     else:
-        print(f"{n} is NOT a prime number!")
-else:
-    print(f"{n} is NOT a prime number!")  # 2 미만의 수는 소수가 아님
-#dan = input("Intput dan : ")  함수는 문자열 처리라 9 x2 = 99 이런식으로 출력됨
+        return False
+    return True
 
-#dan =  int(input("input dan :"))
-#for i in range(1, 10, 1):
-#    print(f"{dan}*{i}= {dan*i}")
-
-
-
-#for dan in range(2, 10, 1):
-#    for i in range(1, 10 , 1):
-#        print(f"{dan}*{i}={dan*i}")
-
+# main
+#help(abs)
+help(is_prime)
 n = int(input("Input number : "))
-is_prime = True
-if n >= 2:
-    #for i in range(2, n):
-    for i in range(2, int(n**0.5)+1):
-        if n % i == 0:
-            is_prime = False  #count = count + 1
-            break
-        print(i, end=' ')
-else:
-    is_prime = False
 
-#if count == 0:
-if is_prime:
+if is_prime(n):  # function call
     print(f"{n} is prime number")
 else:
     print(f"{n} is NOT prime number!")
