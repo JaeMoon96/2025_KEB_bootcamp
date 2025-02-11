0ba3f1d2d3be550f2439f3a79f0d943753430203
JaeMoon <ljm96040@gmail.com>
2025-02-11 16:50:11 +0900

    v1.1 day02 prime number

--- day02.py
+++ day02.py
@@ -4,18 +4,25 @@
 
 
 
+# 1) for -> while  v1.1
+# 2) while 구문으로 구간 소수를 출력하는 프로그램을 작성 v1.2
+# 3) ** 대신 pow 함수 v1.3
+
+
+
 def is_prime(num) -> bool:
     """
     A function that returns True if it is a prime number and False if it is not a prime number
     :param num: integer number
     :return: boolean type
     """
-    if num >= 2:
-        i = 2
-        while i <= int(num ** 0.5):
-            if num % i == 0:
-                return False
-            i += 1
+    i = 2
+    while i <= int(num ** 0.5):
+        if num % i == 0:
+            return False
+        i = i + 1
+
+        return True
 
 # main
 #help(abs)
