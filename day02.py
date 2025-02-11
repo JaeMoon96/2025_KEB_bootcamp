ff184fa57f5b5a04e9482aab9b87bf4263acf18c
JaeMoon <ljm96040@gmail.com>
2025-02-11 17:38:50 +0900

    v1.4 day02 prime number

--- day02.py
+++ day02.py
@@ -3,7 +3,13 @@
 # v1.2) Write a program that receives two numbers and outputs only prime numbers between the two numbers. Then, use only the while statement and include the two numbers entered.
 # v1.3) Rewrite the code using the power function instead of the ** operator.
 # v1.4) Make my_pow custom function instead of ** operator, power function and make it work.
-
+def my_pow(b, e) -> float:
+    """
+       A user-defined function that receives a base and exponent and returns the power result in the form of a real number
+       :param b: base number
+       :param e: exponent
+       :return: the power result in the form of a real number
+       """
 def is_prime(num) -> bool:
     """
     A function that returns True if it is a prime number and False if it is not a prime number
@@ -12,8 +18,8 @@ def is_prime(num) -> bool:
     """
     if num >= 2:
         i = 2
-        #while i < (int(num ** 0.5) + 1):
-        while i < (int(pow(num, 0.5)) + 1):
+        #while i < (int(my_pow(num, 0.5)) + 1):
+        while i*i < num+1:
             if num % i == 0:
                 return False
             i = i + 1
@@ -21,6 +27,8 @@ def is_prime(num) -> bool:
         return False
     return True
 
+
+#print(my_pow(2, 9))
 numbers = input("Input number : ").split()  # ex) 900 1000
 n1 = int(numbers[0])
 n2 = int(numbers[1])
