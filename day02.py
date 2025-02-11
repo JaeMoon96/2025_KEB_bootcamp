d0dd6f9ae34cc3ece574c7d6f588fb0f9bbb130f
JaeMoon <ljm96040@gmail.com>
2025-02-11 10:34:40 +0900

    v0.2

---
+++ day02.py
@@ -0,0 +1,10 @@
+#dan = input("Intput dan : ")  함수는 문자열 처리라 9 x2 = 99 이런식으로 출력됨
+dan =  int(input("input dan :"))
+for i in range(1, 10, 1):
+    print(f"{dan}*{i}= {dan*i}")
+
+
+
+#for dan in range(2, 10, 1):
+#    for i in range(1, 10 , 1):
+#        print(f"{dan}*{i}={dan*i}")
