b2664c907c5af2bf505d22d302785a972e709afd
JaeMoon <ljm96040@gmail.com>
2025-02-11 13:07:46 +0900

    v0.9 day02 prime number

--- day02.py
+++ day02.py
@@ -1,3 +1,38 @@
+from operator import truediv
+
+n = int(input("Input number : "))
+is_prime = True
+
+if n >=2:
+     for i in range(2 , n):
+         if n % i == 0:
+            is_prime = False #count = count +1
+            break
+
+        else
+        is_prime = True
+
+        if count == 0:
+            print(f"{n} is prime number")
+        else:
+            print(f"{n} is NOT prime number!")
+
+
+n = int(input("Input number: "))
+is_prime = True
+
+if n >= 2:
+    for i in range(2, n):
+        if n % i == 0:
+            is_prime = False
+            break  # 약수가 발견되면 즉시 종료
+
+    if is_prime:
+        print(f"{n} is a prime number")
+    else:
+        print(f"{n} is NOT a prime number!")
+else:
+    print(f"{n} is NOT a prime number!")  # 2 미만의 수는 소수가 아님
 #dan = input("Intput dan : ")  함수는 문자열 처리라 9 x2 = 99 이런식으로 출력됨
 
 #dan =  int(input("input dan :"))
@@ -10,13 +45,20 @@
 #    for i in range(1, 10 , 1):
 #        print(f"{dan}*{i}={dan*i}")
 
-
-    n = int(input("Input number : "))
-    cout = 0
-    for i in range(1 , n+1):
+n = int(input("Input number : "))
+is_prime = True
+if n >= 2:
+    #for i in range(2, n):
+    for i in range(2, int(n**0.5)+1):
         if n % i == 0:
-            count = count + 1
-        if count == 2:
-            print(f"{n} is prime number")
-        else:
-            print(f"{n} is NOT prime number!")
+            is_prime = False  #count = count + 1
+            break
+        print(i, end=' ')
+else:
+    is_prime = False
+
+#if count == 0:
+if is_prime:
+    print(f"{n} is prime number")
+else:
+    print(f"{n} is NOT prime number!")
