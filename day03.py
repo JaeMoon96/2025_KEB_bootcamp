c30edfbd7c27c3b5ea3ef519903b7f206079f5f9
JaeMoon <ljm96040@gmail.com>
2025-02-11 15:32:58 +0900

    v0.2 phython join split day03

--- day03.py
+++ day03.py
@@ -1,18 +1,13 @@
-university = "Inha\nUniversity!"
-#university = r"Inha\nUniversity!"  # raw string 역슬래쉬 그대로 출력
-#print(university)
+subjects = ["python", "c++", "database"]
+subjects_string = " / ".join(subjects)
+print(subjects_string)
 
-# slicing
-# print(university[:4])   #inha 왼쪽부터 4개
-# print(university[:-11]) #inha 오른쪽부터 11개
-# print(len(university))  #len 문자열의 길이 16
-# print(university[0:len(university)]) #INHA
-                                        #University
-# print(university[:16])
-# print(university[::2])
+numbers = input("FirstNumber SecondNumber : ").split()
+#print(numbers[0] + numbers[1])  # concatenation
+print(int(numbers[0]) + int(numbers[1]))  # arithmetic operation
 
-number1 = input("First number : ")  #5
-number2 = input("Second number : ") #3
-print(number1 + number2)  # concatenation -> 53
-print(number1 * 3)  # duplicate -> 555
-print(number1 + 3)  # TypeError: can only concatenate str (not "int") to str
+# course = "2024 KEB Bootcamp"
+# print(course)
+# #list_course = course.split()
+# list_course = course.split('B')
+# print(list_course)
