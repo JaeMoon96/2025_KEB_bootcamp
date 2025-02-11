960e8c13c0aec456a0e6639343a298c262dd1d5a
JaeMoon <ljm96040@gmail.com>
2025-02-11 15:41:58 +0900

    ãv0.4 python day03

--- day03.py
+++ day03.py
@@ -1,20 +1,6 @@
-course = "* KEB 2024# KEB !Bootcamp KEB...*!#"
-print(course.find('KEB'))
-print(course.rfind('KEB'))
-print(course.index('KEB'))
-print(course.rindex('KEB'))
-print(course.find('Inha'))  # -1 find는 찾는게없으면 -1 -> if문으로 -1출력시 예외처리가능
-print(course.index('Inha'))  # ValueError: substring not found index는 예외
-
-print(course)
-course = course.replace('KEB', 'Inha', 2)
-print(course)
-print(course.strip())
-print(course.strip("!#.*"))
-
-
-# print(course)
-# print(course.replace('KEB', 'Inha'))
-# print(course)
-# course = course.replace('KEB', 'Inha')
-# print(course)
+subjects = "python c++ database linux"
+subject = input("수강신청과목 입력 : ")
+if subjects.find(subject) != -1:
+    print(f"해당 과목이 존재합니다. 위치는 {subjects.find(subject)}번 인덱스입니다.")
+else:
+    print('해당과목이 존재하지 않습니다')
