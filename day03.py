93f3729684ff8fd4573e8fca744206fa9408cbf9
JaeMoon <ljm96040@gmail.com>
2025-02-12 14:08:47 +0900

    v0.2 python day3 comprehension

--- day03.py
+++ day03.py
@@ -1,24 +1,20 @@
-mport copy  # copy 모듈을 불러옴
+# squares = list()
+# squares = [] 
+# squares.append(1*1)
+# squares.append(2*2)
+# squares.append(3*3)
+# squares.append(4*4)
+# squares.append(5*5)
+# print(squares)
+# 
+# squares = list()
+# for i in range(1, 6, 1):
+#     squares.append(i*i)
+# print(squares)
 
-# subjects 리스트 생성 (얕은 복사와 깊은 복사의 차이를 확인하기 위해 중첩 리스트 포함)
-subjects = ["a", ["b", "c"], "d"]
+#  list comprehension(축약)
+#  squares = [i*i for i in range(1, 6, 1)]
+#  print(squares)
 
-# 변수 할당 (같은 객체를 참조)
-a = subjects  # a는 subjects와 같은 객체를 참조 (원본과 동일한 주소)
-
-# 얕은 복사(Shallow Copy) - 첫 번째 레벨 요소만 복사, 중첩 리스트는 원본과 공유됨
-b = subjects.copy()  # 얕은 복사 (첫 번째 레벨만 복사됨)
-c = list(subjects)   # list()를 사용한 얕은 복사 (b와 같은 방식)
-d = subjects[:]      # 슬라이싱을 사용한 얕은 복사 (b, c와 같은 방식)
-
-# 깊은 복사(Deep Copy) - 모든 요소를 새로운 객체로 복사
-e = copy.deepcopy(a)  # deepcopy는 중첩된 리스트까지 완전히 새로운 객체로 복사
-
-# 복사된 객체들을 출력 (모두 원본과 동일한 내용을 가짐)
-print(subjects, a, b, c, d, e)
-
-# 원본 리스트의 중첩 리스트 내부 요소 변경
-subjects[1][1] = "x"  # 중첩 리스트의 두 번째 요소 "c"를 "x"로 변경 => e만 deepcopy이므로 영향받지않음
-
-# 변경 후 모든 리스트 출력
-print(subjects, a, b, c, d, e)
+even_squares = [i*i for i in range(1, 6, 1) if i % 2 == 0]
+print(even_squares)
