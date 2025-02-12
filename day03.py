bc4655c24636bbb8e8e3264c2e3bfbc6dbff6123
JaeMoon <ljm96040@gmail.com>
2025-02-12 17:46:26 +0900

    v2.6 day03 assignment

--- day03.py
+++ day03.py
@@ -1,5 +1,5 @@
 #Assignment
-#v2,5) while 안쪽에 하드 코딩된 코드를 함수화 하시오
+#v2,6) while 안쪽에 조건문을 줄이기
 
 import random
 
@@ -24,24 +24,24 @@ print(menu_list)
 #menu = input(f'다음 술 중에 고르세요.\n' f'1) {drinks[0]}   2) {drinks[1]}   'f'3) {drinks[2]}   4) {drinks[3]}   'f'5) {drinks[4]}   6) 아무거나   7) 종료 : ')
 
 while True:
-    menu = input()
-    if menu == '1':
+    menu = int(input(menu_list))
+    if 1 <= menu <= len(drinks):
         print_menu(int(menu)-1)
-    elif menu == '2':
-        print_menu(int(menu)-1)
-    elif menu == '3':
-        print_menu(int(menu)-1)
-    elif menu == '4':
-        print_menu(int(menu)-1)
-    elif menu == '5':
-        print_menu(int(menu) - 1)
-    elif menu == '6':
+    # elif menu == '2':
+    #     print_menu(int(menu)-1)
+    # elif menu == '3':
+    #     print_menu(int(menu)-1)
+    # elif menu == '4':
+    #     print_menu(int(menu)-1)
+    # elif menu == '5':
+    #     print_menu(int(menu)-1)
+    elif menu == 6:
         random_index = random.randint(0, len(drinks)-1)
         #random_index = random.randint(0, 4) 보다는 위의 코드가 메뉴가 업데이트 됐을때 대응하기좋음
         print(f'{drinks[random_index]}에 어울리는 안주는 {snacks[random_index]} 입니다')
         # random_drink = random.choice(drinks_foods_keys)
         # print(f'{random_drink}에 어울리는 안주는 {drinks_foods[random_drink]} 입니다')
-    elif menu == '7':
+    elif menu == 7:
         print(f'다음에 또 오세요')
         break
 
