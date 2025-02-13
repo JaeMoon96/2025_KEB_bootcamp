9af56a50422bca548422a69ceba14c2aa8f578ce
JaeMoon <ljm96040@gmail.com>
2025-02-13 09:38:16 +0900

    ver2.7 day03 assignment

--- day03.py
+++ day03.py
@@ -3,8 +3,12 @@
 
 import random
 
+# d_s_p = {"위스키" : ['초콜릿', 50_000]}
+# print(d_s_p["위스키"][1])
+
 drinks = ["위스키", "와인", "소주", "고량주"]
 snacks = ["초콜릿", "치즈", "삼겹살", "양꼬치"]
+prices = [50000, 30000 , 5000, 7500 ]
 
 def print_menu(n):
     print(f'{drinks[n]}에 어울리는 안주는 {snacks[n]} 입니다')
@@ -12,15 +16,17 @@ def print_menu(n):
 
 drinks.append("사케")
 snacks.append("광어회")
+prices.append(25000)
 snacks[0] = "낙곱새"
-#drinks.append("데킬라")
-#snacks.append("소금")
+drinks.append("데킬라")
+snacks.append("소금")
+prices.append(50000)
 
 menu_list = '다음 술중에 고르세요.\n'
 for i in range(len(drinks)):
     menu_list = menu_list + f'{i+1}) {drinks[i]}  '
 menu_list = menu_list + f'{len(drinks)+1}) 아무거나  {len(drinks)+2}) 종료 : '
-print(menu_list)
+
 #menu = input(f'다음 술 중에 고르세요.\n' f'1) {drinks[0]}   2) {drinks[1]}   'f'3) {drinks[2]}   4) {drinks[3]}   'f'5) {drinks[4]}   6) 아무거나   7) 종료 : ')
 
 while True:
@@ -35,7 +41,7 @@ while True:
     #     print_menu(int(menu)-1)
     # elif menu == '5':
     #     print_menu(int(menu)-1)
-    elif menu == len(drinks)+1 :
+    elif menu == len(drinks) +1 :
         random_index = random.randint(0, len(drinks)-1)
         #random_index = random.randint(0, 4) 보다는 위의 코드가 메뉴가 업데이트 됐을때 대응하기좋음
         print(f'{drinks[random_index]}에 어울리는 안주는 {snacks[random_index]} 입니다')
