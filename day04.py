9da2e236fca6053a83958fc61609cc4e04632e15
JaeMoon <ljm96040@gmail.com>
2025-02-13 14:28:20 +0900

    ver3.4 day04 decorator

--- day04.py
+++ day04.py
@@ -2,7 +2,16 @@
 # Open Closed Principle : 개방 폐쇄 원칙 (확장에는 열려 있고 수정에는 닫혀있는 원칙)
 import time
 
+def time_decorator(func):
+    def wrapper(*arg):
+        s = time.time()
+        r = func(*arg)
+        e = time.time()
+        print(f'실행시간 : {e - s}초')
+        return r
+    return wrapper
 
+@time_decorator
 def factorial_repetition(n) -> int:
     result = 1
     for i in range(2, n+1):
