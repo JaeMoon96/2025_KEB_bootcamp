41b4669b5dc9b5bc78016cae8b5cbb329ccf1168
JaeMoon <ljm96040@gmail.com>
2025-02-13 15:28:45 +0900

    v3.5 day04 ocp decorator

--- day04.py
+++ day04.py
@@ -2,6 +2,7 @@
 # Open Closed Principle : 개방 폐쇄 원칙 (확장에는 열려 있고 수정에는 닫혀있는 원칙)
 import time
 
+
 def time_decorator(func):
     def wrapper(*arg):
         s = time.time()
@@ -11,15 +12,16 @@ def time_decorator(func):
         return r
     return wrapper
 
-@time_decorator
+#@time_decorator
 def factorial_repetition(n) -> int:
     result = 1
     for i in range(2, n+1):
         result = result * i
     return result
 
+
 number = int(input())
-s = time.time()
+ft = time_decorator(factorial_repetition)
+print(f"{number}! = {ft(number)}")
+number = int(input())
 print(f"{number}! = {factorial_repetition(number)}")
-e = time.time()
-print(e-s)
