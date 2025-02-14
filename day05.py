7ff3cfb0f7c99b4e7db3592f991f69d590cb7001
JaeMoon <ljm96040@gmail.com>
2025-02-14 11:28:09 +0900

    v4.1 day05 countdown

--- day05.py
+++ day05.py
@@ -27,32 +27,54 @@
 
 #반복문은 메모리 사용 최소화 (제너레이터 이용) 일반적으로 재귀함수를 쓰는경우는 계속 함수를 호출해야 하므로 속도가 느림
 
-def fibonacci_recursion(n) -> int:
-    """
-    피보나치 수 계산함수 (재귀함수 버전)
-    :param n:
-    :return: 피보나치 계산 결과 값
-    """
-    if n <= 0:
-        return 0
-    elif n == 1:
-        return 1
+# def fibonacci_recursion(n) -> int:
+#     """
+#     피보나치 수 계산함수 (재귀함수 버전)
+#     :param n:
+#     :return: 피보나치 계산 결과 값
+#     """
+#     if n <= 0:
+#         return 0
+#     elif n == 1:
+#         return 1
+#     else:
+#         return fibonacci_recursion(n-2) + fibonacci_recursion(n-1)
+#
+#
+# def fibonacci_loop(n) -> int:
+#     """
+#     피보나치 수 계산함수 (반복문 버전)
+#     :param n:
+#     :return: 피보나치 계산 결과 값
+#     """
+#     n_list=[0 ,1]
+#     for i in range(n+1):
+#         n_list.append(n_list[i] + n_list[i + 1])
+#
+#     return n_list[n]
+#
+# n = int(input())
+# print(fibonacci_loop(n))
+# print(fibonacci_recursion(n))
+
+def countdown_loop(n):
+    for i in range(n, -1, -1):
+        if i == 0:
+            print("펑~")
+        else:
+            print(i)
+
+
+def countdown_recursion(n):
+    if n < 0:
+        return
+    if n == 0:
+        print("펑!")
     else:
-        return fibonacci_recursion(n-2) + fibonacci_recursion(n-1)
+        print(n)
+    countdown_recursion(n-1)
 
-
-def fibonacci_loop(n) -> int:
-    """
-    피보나치 수 계산함수 (반복문 버전)
-    :param n:
-    :return: 피보나치 계산 결과 값
-    """
-    n_list=[0 ,1]
-    for i in range(n+1):
-        n_list.append(n_list[i] + n_list[i + 1])
-
-    return n_list[n]
 
 n = int(input())
-print(fibonacci_loop(n))
-print(fibonacci_recursion(n))
+#countdown_loop(n)
+countdown_recursion(n)
